fbf615fecf752a5f1cad741a836c87a4864ca29a
Dariels A. Valladares Martinez <darielsmartinez926@gmail.com>
2024-10-29 13:24:36 +0100

    Refactorizar archivo scrollReveal.js y ajustar configuración de animación

--- src/scrollReveal.js
+++ src/scrollReveal.js
@@ -2,16 +2,15 @@
 
 import ScrollReveal from "scrollreveal";
 
-// Configuración de ScrollReveal
 export const initScrollReveal = () => {
   ScrollReveal().reveal(".elemento-revelar", {
-    duration: 1500, // Duración más corta para un efecto más dinámico
-    origin: "bottom", // Posición inicial de la animación
-    distance: "100px", // Mayor distancia para que sea más visible la subida
-    opacity: 0, // Inicia desde opacidad cero para un efecto de fade-in
-    scale: 0.85, // Efecto de "zoom-in" al aparecer
-    easing: "cubic-bezier(0.45, 0.58, 0, 1)", // Efecto de aceleración suave
-    interval: 140, // Intervalo de tiempo entre cada animación (en milisegundos)
-    reset: true, // Evita la animación al volver a hacer scroll
+    duration: 1500,
+    origin: "bottom",
+    distance: "100px",
+    opacity: 0,
+    scale: 0.85,
+    easing: "cubic-bezier(0.45, 0.58, 0, 1)",
+    interval: 140,
+    reset: true,
   });
 };
